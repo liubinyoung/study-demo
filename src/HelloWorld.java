c833bb88665e1e90fd8356466cbe2daa0adcd6a0
liubinyoung <chian.liubingyang@outlook.com>
2020-12-14 21:38:05 +0800

    Git2

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,7 +1,8 @@
 public class HelloWorld{
     
     public static void main(String args[]){
-        System.out.println("Hello Git!")
+        System.out.println("Hello Git!")!
+	System.out.println("Hello Git2!");
     }
  
 }
