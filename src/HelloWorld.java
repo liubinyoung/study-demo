abe93053c5091022f31438c5460abc24f7600b05
zqm0906 <839854231@qq.com>
2020-12-14 22:29:33 +0800

    已经修改

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,8 +1,9 @@
 public class HelloWorld{
     
     public static void main(String args[]){
-        System.out.println("Hello Git!")
+        System.out.println("Hello Git!");
         System.out.println("Hello 刘冰阳");
+	System.out.println("张起鸣到此一游！！！！")
     }
 
 
